bd56c0777dc003a3aa748f339ef283e481005848
Shady7781 <troudishedy6@gmail.com>
2021-10-06 09:35:04 +0200

    add timesheet project

---
+++ src/test/java/tn/esprit/spring/test/TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests.java
@@ -0,0 +1,18 @@
+package tn.esprit.spring.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+public class TimesheetSpringBootCoreDataJpaMvcRest1ApplicationTests {
+    @Test
+    public void contextLoads() {
+
+    }
+
+
+}
